28a6248a6c51b69b106c1288142d06b23b321b2b
SumYg <61381909+SumYg@users.noreply.github.com>
2025-07-21 17:01:23 -0700

    Change build dir

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  outDir: 'docs',  // or wherever you want the built files
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
